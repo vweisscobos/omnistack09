3ad7485f61ddb6f205ce0ac77046b9b313d721a8
Vinicius Weiss <v.weisscobos@gmail.com>
2019-10-03 10:49:05 -0300

    routes config

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -1,7 +1,20 @@
 // index, show, store, update, destroy
 
+const User = require('../models/User');
+
 module.exports = {
-  store(req, res) {
-    return res.json({ message: 'hello world!'});
-  }
+
+  async store(req, res) {
+    const { email } = req.body;
+
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      user = await User.create({ email });
+    }
+
+    return res.json(user);
+  },
+
+
 };
