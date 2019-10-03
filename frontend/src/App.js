b7fa722bdc0879e5cceb4449ba0e69bd95ac59e6
Vinicius Weiss <v.weisscobos@gmail.com>
2019-10-03 14:44:46 -0300

    login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import api from './services/api';
+
+import logo from './assets/logo.svg';
 
 function App() {
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+
+    const response = await api.post('/sessions', { email });
+
+    console.log(response);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={'container'}>
+
+      <img src={logo} alt={'airCnC'} />
+
+      <div className={'content'}>
+        <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para a sua empresa.</p>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">E-MAIL</label>
+          <input
+            id={'email'}
+            type={'email'}
+            placeholder={'Seu melhor e-mail'}
+            value={email}
+            onChange={evt => setEmail(evt.target.value)}
+          />
+
+          <button className={'btn'}>Entrar</button>
+        </form>
+      </div>
     </div>
   );
 }
